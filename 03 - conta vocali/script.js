5b6434eb53a895825ea37e162da2adade7139053
salvoparisi <psalvo065@gmail.com>
2024-10-07 15:30:13 +0200

    add conta vocali

--- 03 - conta vocali/script.js
+++ 03 - conta vocali/script.js
@@ -1,13 +1,32 @@
 /* Scrivi una funzione che accetti una stringa e restituisca il numero di vocali contenute al suo interno */
 
 const word = 'javascript';
-
+const vocals = ["a", "e", "i", "o", "u"];
+console.log(vocals);
 
 // Dichiara la funzione qui.
 
+function vocalsFind(newWord){
+    let num=0;
+    let newVocals=[];
+    let newVocalsClean=[];
+    newWordSplit=newWord.split("")
+    for(let i=0; i<newWordSplit.length; i++){
+        for(let z=0; z<5; z++){
+            if(newWordSplit[i]==vocals[z]){
+                num++;
+                newVocals[i]=vocals[z];
+            }
+        }
+    }
+    newVocalsClean=newVocals.filter(Element=>Element)
+    
+    console.log(`${num} (${newVocalsClean})`);
+    
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+vocalsFind(word)
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
