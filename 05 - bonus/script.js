2d2d1e24aba87aeb8990c5ebc3e3441cd91fdbd5
salvoparisi <psalvo065@gmail.com>
2024-10-07 15:53:22 +0200

    add bonus

--- 05 - bonus/script.js
+++ 05 - bonus/script.js
@@ -10,9 +10,22 @@ const name = 'Mario';
 
 // Dichiara la funzione qui.
 
+function greetings(newName){
+    let salutiMattina="buongiorno";
+    let salutiPomeriggio="buon pomeriggio"
+    let salutiSera="buonasera"
+    let ore=new Date().getHours()
+    if(ore>6 && ore<13){
+        console.log(salutiMattina+" "+newName);
+    }else if(ore>12 && ore<18){
+        console.log(salutiPomeriggio+" "+newName);
+    }else if(ore>17 && ore<7){
+        console.log(salutiSera+" "+newName);
+    }
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+greetings(name)
 
 //Risultato atteso se si passa 'Mario' alle 18: // Buonasera Mario.
