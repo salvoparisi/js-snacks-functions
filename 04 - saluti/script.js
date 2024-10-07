69876b8c53af8affb8f26e10906c77936a9b0ee1
salvoparisi <psalvo065@gmail.com>
2024-10-07 15:36:31 +0200

    add saluti

--- 04 - saluti/script.js
+++ 04 - saluti/script.js
@@ -5,9 +5,15 @@ const name = 'Mario';
 
 // Dichiara la funzione qui.
 
+function greetings(newName){
+    let saluti="ciao";
+    let sumSaluti=saluti+" "+newName;
+    return sumSaluti
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
+console.log(greetings(name));
 
 
 //Risultato atteso se si passa 'Mario': // ciao Mario
