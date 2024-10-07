22ffc3112d18b79856c539845cb8b8669561c01c
salvoparisi <psalvo065@gmail.com>
2024-10-07 14:45:03 +0200

    add iniziali

--- 01 - Iniziali/script.js
+++ 01 - Iniziali/script.js
@@ -2,12 +2,21 @@
 
 const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
-
 // Dichiara la funzione qui.
 
+function initialName(array){
+    for(let i=0; i<array.length; i++){
+        array[i] = array[i].charAt(0);
+    }
+    return array;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
+console.log(initialName(names));
 
 
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]
+
+
+
